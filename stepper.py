1c95a7753fab138611b0d554dc8a8cf25641f716
natchapol2347 <pupreez@gmail.com>
2020-11-25 20:45:15 +0700

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 import keyboard
 used_pin = (8, 10, 12,16)
-used_pin_2 = (2, 3, 4, 17)
+used_pin_2 = (3, 5, 7, 11)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(used_pin, GPIO.OUT)
-GPIO.setup(used_pin, GPIO.OUT)
+GPIO.setup(used_pin_2, GPIO.OUT)
 
 step = [[1, 0, 0, 0],
         [1, 1, 0, 0],
@@ -16,9 +16,12 @@ step = [[1, 0, 0, 0],
         [0, 0, 0, 1],
         [1, 0, 0, 1]
        ]
+x_step = 0
 while True:
-    for i in step:
-        for j in range(4):
-            #GPIO.output(used_pin[j], i[j])
-            print(used_pin[j], i[j])
+    for j in range(4):
+        #GPIO.output(used_pin[j], i[j])
+        print(used_pin[j], step[i][j])
+    x_step += 1
+    if x_step == 8:
+        x_step = 0
 
